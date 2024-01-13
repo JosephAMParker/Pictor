a988338829a6f9cb46beda65a3694b81ae768667
Joe <Joeyparker47@gmail.com>
2024-01-13 01:02:38 -0800

    add GA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';   
 import Pictor from './pages/Pictor';
+import { Helmet } from 'react-helmet';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<div><Home /></div>} />
-        <Route path="pictor" element={<div><Pictor /></div>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HC7CCHZVMP"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HC7CCHZVMP');
+          `}
+        </script>
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<div><Home /></div>} />
+          <Route path="pictor" element={<div><Pictor /></div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
