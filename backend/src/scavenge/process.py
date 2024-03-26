11f865dc411cf22d891ee526b0cb1569051ac6f7
Joe <joeyparker47@gmail.com>
2024-03-25 17:09:56 -0700

    add scavange page

---
+++ backend/src/scavenge/process.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import scavenge_classes  # Importing the classes from config.py
+
+def predict_class(image_path):
+    # Load the saved model
+    model = tf.keras.models.load_model('landmark_classifier.h5')
+
+    # Preprocess the new image (resize to 224x224 and rescale pixel values)
+    image = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = image / 255.0  # Rescale pixel values to [0, 1]
+    image = tf.expand_dims(image, axis=0)  # Add batch dimension
+
+    # Make predictions on the new image
+    predictions = model.predict(image)
+    predicted_class_index = tf.argmax(predictions, axis=-1)
+    predicted_class_index_value = predicted_class_index.numpy()[0]
+
+    predicted_class = scavenge_classes.landmarks[predicted_class_index_value]
+    return predicted_class
